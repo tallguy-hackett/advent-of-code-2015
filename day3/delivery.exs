32e0e10494a2e823ea1987eb7f7a6906b5b7f272
Dave Hackett <tallguy.hackett@gmail.com>
2015-12-12 22:25:29 -0700

    Day 3 part 2 Santa and Robot Santa

--- day3/delivery.exs
+++ day3/delivery.exs
@@ -28,9 +28,45 @@ defmodule Delivery do
     |> Enum.uniq()
     |> Enum.count()
   end
+
+  def get_santa_and_robot_unique_count(filename) do
+    {santa_path, robot_path} =
+    File.read!(filename)
+    |> String.to_char_list()
+    |> move_part_2({0, 0}, {0, 0}, [{0, 0}], [{0, 0}])
+
+    combined_paths = santa_path ++ robot_path
+    combined_paths
+    |> Enum.uniq()
+    |> Enum.count()
+  end
+
+  defp move_part_2([], _, _, santa_path, robot_path) do
+    {Enum.reverse(santa_path), Enum.reverse(robot_path)}
+  end
+  defp move_part_2([santa_instr, robot_santa_instr | rest_of_instructions],
+                    current_santa,
+                    current_robot,
+                    santa_path,
+                    robot_path) do
+    new_santa_location = move(santa_instr, current_santa)
+    new_robot_location = move(robot_santa_instr, current_robot)
+    move_part_2(rest_of_instructions,
+                new_santa_location,
+                new_robot_location,
+                [new_santa_location | santa_path],
+                [new_robot_location | robot_path])
+  end
+
+  defp move(?<, {x, y}), do: {x-1, y}
+  defp move(?>, {x, y}), do: {x+1, y}
+  defp move(?^, {x, y}), do: {x, y+1}
+  defp move(?v, {x, y}), do: {x, y-1}
 end
 
 Delivery.get_house_path("delivery_instructions.txt")
 |> IO.inspect()
 
 IO.puts "Santa visited #{Delivery.get_unique_home_count("delivery_instructions.txt")} unique homes"
+
+IO.puts "Santa and Robot Santa visited #{Delivery.get_santa_and_robot_unique_count("delivery_instructions.txt")} unique homes"
